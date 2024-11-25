284f32fba739ff85fc74b8c51467a003dccb7094
korohangler <evgenydmitriev901@gmail.com>
2024-11-25 19:57:22 +0100

    update

---
+++ cpp_utilities/include/reflection/type_name.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <string_view>
+
+namespace cpp_utilities
+{
+namespace reflection
+{
+
+namespace detail
+{
+
+} //-- detail
+
+template<typename T>
+std::string_view get_type_name()
+{
+
+}
+
+} //-- reflection
+} //-- cpp_utilities
